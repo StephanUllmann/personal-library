223dbc80cd2bc6978951b5bdb999d044f6c9b046
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-02-14 13:30:05 +0100

    removed sigterm listener

--- personal-library-api/server.js
+++ personal-library-api/server.js
@@ -31,10 +31,10 @@ process.on('unhandledRejection', (err) => {
   });
 });
 
-process.on('SIGTERM', () => {
-  console.log('SIGTERM signal received: closing HTTP server');
-  server.close(() => {
-    console.log('HTTP server closed');
-    process.exit(0);
-  });
-});
+// process.on('SIGTERM', () => {
+//   console.log('SIGTERM signal received: closing HTTP server');
+//   server.close(() => {
+//     console.log('HTTP server closed');
+//     process.exit(0);
+//   });
+// });
