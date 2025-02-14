fe0b27925c973142ee6c10a9e5ec8b977d79c193
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-02-14 13:12:53 +0100

    sigterm

--- personal-library-api/server.js
+++ personal-library-api/server.js
@@ -35,5 +35,6 @@ process.on('SIGTERM', () => {
   console.log('SIGTERM signal received: closing HTTP server');
   server.close(() => {
     console.log('HTTP server closed');
+    process.exit(0);
   });
 });
